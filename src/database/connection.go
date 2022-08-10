658507e9b0961ac0483ebe8f80477393c7e0b23b
Nguyễn Tiến Cường <cuong.nguyen@apps-cyclone.com>
2022-08-10 09:21:40 +0700

    Update auto create table.

--- src/database/connection.go
+++ src/database/connection.go
@@ -3,18 +3,20 @@ package database
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"main/src/modules/user/models"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("mysql", "root:tiencuong@123@tcp(localhost:3306)/wolfden")
+	db, err := gorm.Open("mysql", "root:tiencuong@123@tcp(localhost:3306)/test")
 
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
 
-	//   database.AutoMigrate(&Book{})
+	db.AutoMigrate(models.User{})
 
-	DB = database
+	DB = db
 }
---
+++ src/modules/user/models/user.model.go
@@ -0,0 +1,7 @@
+package models
+
+type User struct {
+	UserId   uint   `json:"userId" gorm:"primary_key;autoIncrement;column:UserId"`
+	UserName string `json:"userName" gorm:"column:UserName"`
+	Password string `json:"password" gorm:"column:Password"`
+}
